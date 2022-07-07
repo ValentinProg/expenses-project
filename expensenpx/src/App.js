c0886c69fd9312f9ee35ebbc9d6dcfc53f3a2248
Valentin <V.Kovalenko.prog@gmail.com>
2022-07-07 16:33:10 +0300

    add ExpenseItem

--- expensenpx/src/App.js
+++ expensenpx/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpenseItem from "./components/ExpenseItem/ExpenseItem";
 
 function App() {
   return (
     <div className="App">
-      d
+        <h1>Let start!!</h1>
+        <ExpenseItem/>
     </div>
   );
 }
