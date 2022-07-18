902ee46c2c2350cf8a315468a14b2b658cfd5331
Valentin <V.Kovalenko.prog@gmail.com>
2022-07-18 11:46:39 +0300

    add ChartBar

--- expensenpx/src/components/Expenses/Expenses.jsx
+++ expensenpx/src/components/Expenses/Expenses.jsx
@@ -1,8 +1,10 @@
 import React, {useState} from "react";
 import Card from "../UI/Card/Card";
-import ExpenseItem from "./ExpenseItem/ExpenseItem";
+
 import './Expenses.css'
 import ExpensesFilter from "./ExpensesFilter/ExpensesFilter";
+import ExpensesList from "./ExpensesList/ExpensesList";
+import ExpensesChart from "./ExpensesChart/ExpensesChart";
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2020')
@@ -18,19 +20,14 @@ const Expenses = (props) => {
       return expense.date.getFullYear().toString() === filteredYear
     })
 
+  
+
   return (
     <div>
     <Card className="expenses">
       <ExpensesFilter selected ={filteredYear} onChangeFilter={filterChangeHandler}/>
-      {filteredExpenses.length === 0 ? <p>No expenses found.</p> : filteredExpenses.map(expense => 
-        <ExpenseItem
-        key={expense.id}  
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-        />
-      )}
-    
+      <ExpensesChart expenses = {filteredExpenses}/>
+      <ExpensesList items={filteredExpenses}/>
     </Card>
     </div>
   );
--- expensenpx/src/components/NewExpense/ExpenseForm/ExpenseForm.jsx
+++ expensenpx/src/components/NewExpense/ExpenseForm/ExpenseForm.jsx
@@ -47,12 +47,13 @@ const ExpenseForm = (props) => {
           <label>Amount</label>
           <input type="number" value={enteredAmount} onChange={amountChangeHandler}  min="0.01" step="0.01" />
         </div>
-        <div className="new-expense__control">
+        <div className="new-expense__control"> 
           <label>Date</label>
           <input type="date"  value={enteredDate} onChange={dateChangeHandler} min="2019-01-01" max="2022-12-31" />
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>Cancel</button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
